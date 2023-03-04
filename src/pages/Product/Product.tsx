6af30b98231ada174de7b0fda238af5cd5b42a7c
kristinkaklyap <kristinka.klyap@gmail.com>
2023-03-04 15:44:15 +0100

    add product page

---
+++ src/pages/Product/Product.tsx
@@ -0,0 +1,94 @@
+import { useParams } from 'react-router-dom';
+import useFetch from '../../hooks/use-fetch.hook';
+import Container from '../../components/Container/Container';
+import classes from './Product.module.scss';
+import { useState } from 'react';
+
+type ImageProps = {
+  id: number;
+  attributes: {
+    url: string;
+  }
+
+}
+
+interface ProductInterface {
+  id: number;
+  attributes: {
+    images: {
+      data: ImageProps[]
+    },
+    description: string;
+    title: string;
+    price: number;
+    price_old: number;
+  };
+}
+
+const IMG_PREFIX = process.env.REACT_APP_UPLOAD_URL;
+
+const Product = () => {
+  const [selectedImg, setSelectedImg] = useState(0);
+  const [quantity, setQuantity] = useState(1)
+
+  const { id } = useParams();
+
+  const { data, isError, isLoading } = useFetch<ProductInterface>(
+    `/products/${id}?populate=*`
+  );
+
+  const images = data?.attributes.images.data.map((img) => `${IMG_PREFIX}${img.attributes.url}`);
+  const addToCartHandler = () => {
+    console.log('add to cart');
+  };
+  return (
+    <div className={classes.product_teaser}>
+      {data &&
+
+
+        <Container>
+          <div className={classes.product}>
+            {
+              images &&
+              <div className={classes.product__images__container}>
+                <div className={classes.product__images}>
+                  {images.map((item, index) => <img src={item} key={index}
+                                                    onClick={() => setSelectedImg(index)} />)}
+
+                </div>
+                <div className={classes.product__image}>
+                  <img src={images[selectedImg]} />
+                </div>
+              </div>
+            }
+
+
+            <div className={classes.product__info}>
+              <h1 className={classes.product__name}>{data.attributes.title}</h1>
+              <p className={classes.product__description}>{data.attributes.description}</p>
+              <div className={classes.actions}>
+                <div className={classes.price_container}>
+                  {data.attributes.price_old &&
+                    <span className={classes['product__price--old']}>{data.attributes.price_old} PLN</span>}
+                  <span className={classes.product__price}>{data.attributes.price} PLN</span>
+                </div>
+                <div className={classes.add_to_cart}>
+                  <div className='quantity'>
+                    <button onClick={() => setQuantity(prevState => prevState === 1 ? 1 : prevState - 1)}>-</button>
+                    {quantity}
+                    <button onClick={() => setQuantity(prevState => prevState + 1)}>+</button>
+                  </div>
+                  <button className={classes.add_to_cart_btn} onClick={addToCartHandler}>
+                    Add to cart
+                  </button>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </Container>
+      }
+    </div>
+  );
+};
+export default Product;
